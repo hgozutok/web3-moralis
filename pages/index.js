edf283611aeb27d385b37fd7bf09154edb740905
Hugo <huseyin@gozutok.info>
2022-05-09 11:43:35 +0300

    .

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
             size="lg"
             onClick={() =>
               authenticate({
-                rovider: "walletconnect",
+                provider: "walletconnect",
                 mobileLinks: [
                   "rainbow",
                   "metamask",
